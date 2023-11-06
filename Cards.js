83385f5b8a509fedf89053ffa7aa0b5194617e29
hcrj99 <hcrj99@hotmail.com>
2023-11-05 19:52:27 -0500

    rute images fixed

--- Cards.js
+++ Cards.js
@@ -2,93 +2,93 @@ let cardHealt = [];
 let cardFurniture = [];
 
 cardHealt.push({
-    image: '/assets/img/products/toothbrush.jpg',
+    image: './assets/img/products/toothbrush.jpg',
     title: 'Cepillo de bambú',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 cardHealt.push({
-    image: '/assets/img/products/hand-cream.jpeg',
+    image: './assets/img/products/hand-cream.jpeg',
     title: 'Crema para manos',
     description: 'Crema hidratante con eucalipto ayuda al cuidado de la piel.'
 });
 
 cardHealt.push({
-    image: '/assets/img/products/serum.jpg',
+    image: './assets/img/products/serum.jpg',
     title: 'Serum hidratante',
     description: 'Sérum hecho a base de extractos naturales y ácido hialurónico.'
 });
 
 cardHealt.push({
-    image: '/assets/img/products/organic-kit.jpg',
+    image: './assets/img/products/organic-kit.jpg',
     title: 'Kit de viaje ',
     description: 'Incluye peine y cepillo de bambú, jabón orgánico de eucalipto y shampoo tamaño de viaje.'
 });
 
 cardHealt.push({
-    image: '/assets/img/products/organic-soap.jpg',
+    image: './assets/img/products/organic-soap.jpg',
     title: 'Set de jabones',
     description: 'Paquete de 2 unidades jabón de carbón activado con coco.'
 });
 
 cardHealt.push({
-    image: '/assets/img/products/guasha-kit.jpg',
+    image: './assets/img/products/guasha-kit.jpg',
     title: 'Kit de skincare',
     description: 'Incluye 2 rodillos y una gua sha.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/wood-chair.jpg',
+    image: './assets/img/furniture/wood-chair.jpg',
     title: 'Silla de bambú',
     description: 'Disponible en 2 colores: blanco y negro, acabado de bambú.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/small-table.jpeg',
+    image: './assets/img/furniture/small-table.jpeg',
     title: 'Banco pequeño',
     description: 'Ideal para agregar un toque de estilo a tus espacios.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/stand-table.jpg',
+    image: './assets/img/furniture/stand-table.jpg',
     title: 'Buró',
     description: 'Elaborado con bambú, cuenta con 2 compartimentos para guardar cosas . '
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/mueble.jpg',
+    image: './assets/img/furniture/mueble.jpg',
     title: 'Cajonera',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/mirror.jpg',
+    image: './assets/img/furniture/mirror.jpg',
     title: 'Espejo de pared',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/desk-lamp.jpg',
+    image: './assets/img/furniture/desk-lamp.jpg',
     title: 'Lámpara bambú',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 cardFurniture.push({
-    image: '/assets/img/furniture/stand.jpg',
+    image: './assets/img/furniture/stand.jpg',
     title: 'Mesita decorativa',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 
 cardFurniture.push({
-    image: '/assets/img/furniture/mini-table.jpg',
+    image: './assets/img/furniture/mini-table.jpg',
     title: 'Lámpara bambú',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
 
 
 cardFurniture.push({
-    image: '/assets/img/furniture/table.jpg',
+    image: './assets/img/furniture/table.jpg',
     title: 'Mesa decorativa',
     description: 'Paquete de 2 unidades hecho 100% de materiales ecológicos.'
 });
